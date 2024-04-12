6b86ad085b5058b1da2545616af4a3904c8c0776
Abhinand K <abhinand8413@gmail.com>
2024-04-12 23:51:52 +0530

    vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline shadow-lg'>hello netflix</h1>
+    </>
+  )
+}
+
+export default App
